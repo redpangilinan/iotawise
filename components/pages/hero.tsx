3bdbb4e0bf74ea3c60cdbee1493c39edab8ae4f4
redpangilinan <redpangilinan715@gmail.com>
2023-08-08 00:57:36 +0800

    feat: update hero image and content

--- components/pages/hero.tsx
+++ components/pages/hero.tsx
@@ -6,15 +6,12 @@ import { cn } from "@/lib/utils"
 export default function HeroHeader() {
   return (
     <section className="lg:items-left container flex flex-col items-center gap-4 pb-12 pt-4 text-center lg:flex-row lg:gap-8 lg:py-20 ">
-      <div className="flex flex-1 flex-col items-center gap-4 lg:items-start lg:gap-8 lg:text-left">
-        <div className="space-y-4">
-          <h1 className="text-4xl font-bold lg:text-6xl">
-            Track daily habits and activities
-          </h1>
-          <h2 className="text-lg font-light text-muted-foreground lg:text-2xl">
-            Monitor your progress and streaks with little effort
-          </h2>
-        </div>
+      <div className="flex flex-1 flex-col items-center gap-4 lg:items-start lg:gap-6 lg:text-left">
+        <h1 className="text-4xl font-bold lg:text-6xl">Build your habits.</h1>
+        <h2 className="text-lg font-light text-muted-foreground lg:text-2xl">
+          Track daily habits and monitor your progress and activity streaks with
+          little effort.
+        </h2>
         <Link
           href="/dashboard"
           className={`w-[10rem] ${cn(buttonVariants({ size: "lg" }))}`}
@@ -24,9 +21,10 @@ export default function HeroHeader() {
       </div>
       <div className="flex flex-1 justify-center lg:justify-end">
         <Image
-          src="/images/hero-img.webp"
-          width={500}
-          height={500}
+          src="/images/hero-img.png"
+          width="600"
+          height="600"
+          priority={true}
           alt="Header image"
         />
       </div>
