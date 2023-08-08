df68bdcb83ab3f61f2a50cfd3c707f2e48b54aef
redpangilinan <redpangilinan715@gmail.com>
2023-08-09 05:24:19 +0800

    feat: create dashboard structure

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,9 @@
+import { getCurrentUser } from "@/lib/session"
+import { dashboardLinks } from "@/config/links"
+
+import { DashboardNav } from "@/components/pages/dashboard/dashboard-nav"
 import Navbar from "@/components/layout/navbar"
 import Footer from "@/components/layout/footer"
-import { getCurrentUser } from "@/lib/session"
 
 interface DashboardLayoutProps {
   children: React.ReactNode
@@ -13,8 +16,21 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <Navbar user={user} />
-      {children}
+      <Navbar
+        user={{
+          name: user?.name,
+          image: user?.image,
+          email: user?.email,
+        }}
+      />
+      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+        <aside className="hidden w-[200px] flex-col md:flex">
+          <DashboardNav items={dashboardLinks.data} />
+        </aside>
+        <main className="flex w-full flex-1 flex-col overflow-hidden">
+          {children}
+        </main>
+      </div>
       <Footer />
     </>
   )
