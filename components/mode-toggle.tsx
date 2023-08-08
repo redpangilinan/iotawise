709531ff5936072ab738d5df0741dd9a8ff2c93a
redpangilinan <redpangilinan715@gmail.com>
2023-08-08 23:09:56 +0800

    fix: remove mode-toggle temp fix (problem is fixed in latest next.js version)

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -3,7 +3,6 @@
 import * as React from "react"
 import { BsMoonStars, BsSun } from "react-icons/bs"
 import { useTheme } from "next-themes"
-import { useState } from "react"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -14,22 +13,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const [isClient, setIsClient] = useState(false)
   const { setTheme } = useTheme()
-
-  React.useEffect(() => {
-    setIsClient(true)
-  }, [])
-
-  if (!isClient) {
-    return (
-      <Button variant="outline" size="icon">
-        <BsSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <BsMoonStars className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        <span className="sr-only">Toggle theme</span>
-      </Button>
-    )
-  }
 
   return (
     <DropdownMenu>
