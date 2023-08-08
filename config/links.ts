5364714e300ce8015650dcc353522d46e6828099
redpangilinan <redpangilinan715@gmail.com>
2023-08-09 05:23:55 +0800

    feat: add dashboard links and move to config

---
+++ config/links.ts
@@ -0,0 +1,38 @@
+import { Navigation } from "@/types"
+
+export const navLinks: Navigation = {
+  data: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Features",
+      href: "/#features",
+    },
+    {
+      title: "Dashboard",
+      href: "/dashboard",
+    },
+  ],
+}
+
+export const dashboardLinks: Navigation = {
+  data: [
+    {
+      title: "Dashboard",
+      href: "/dashboard",
+      icon: "dashboard",
+    },
+    {
+      title: "Activities",
+      href: "/dashboard/activities",
+      icon: "activity",
+    },
+    {
+      title: "Settings",
+      href: "/dashboard/settings",
+      icon: "settings",
+    },
+  ],
+}
