e7be8fdd1163b3ca6ecdae498ae34aa36f57a9ff
redpangilinan <redpangilinan715@gmail.com>
2023-08-13 00:15:24 +0800

    feat: add date format function

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,6 +6,15 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export function formatDate(input: string | number): string {
+  const date = new Date(input)
+  return date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  })
+}
+
 export function absoluteUrl(path: string) {
   return `${env.NEXT_PUBLIC_APP_URL}${path}`
 }
