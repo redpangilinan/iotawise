68005652d2aa91b7fef0d4d75f6cf55a5eb17c91
redpangilinan <redpangilinan715@gmail.com>
2023-08-15 22:19:00 +0800

    refactor: dashboard heading font weight

--- components/pages/dashboard/dashboard-header.tsx
+++ components/pages/dashboard/dashboard-header.tsx
@@ -12,7 +12,7 @@ export function DashboardHeader({
   return (
     <div className="flex flex-col items-center justify-between gap-4 px-2 text-center md:flex-row md:text-left">
       <div className="grid gap-1">
-        <h1 className="font-heading text-2xl md:text-3xl">{heading}</h1>
+        <h1 className="text-2xl font-bold md:text-3xl">{heading}</h1>
         {text && <p className="text-lg text-muted-foreground">{text}</p>}
       </div>
       {children}
