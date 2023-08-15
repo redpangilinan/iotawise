3b3e59a16e14f7c2c50d742d7af0ce1c476df826
redpangilinan <redpangilinan715@gmail.com>
2023-08-15 22:08:27 +0800

    feat: update WIP pages

--- app/dashboard/logs/page.tsx
+++ app/dashboard/logs/page.tsx
@@ -7,8 +7,8 @@ import { DashboardHeader } from "@/components/pages/dashboard/dashboard-header"
 import { Shell } from "@/components/layout/shell"
 
 export const metadata: Metadata = {
-  title: "Dashboard",
-  description: "Monitor your progress.",
+  title: "Logs",
+  description: "View and modify activity logs.",
 }
 
 export default async function SettingsPage() {
@@ -20,7 +20,7 @@ export default async function SettingsPage() {
 
   return (
     <Shell>
-      <DashboardHeader heading="Dashboard" text="Monitor your progress." />
+      <DashboardHeader heading="Logs" text="View and modify activity logs." />
       <div className="grid grid-cols-1 gap-10">Work in progress</div>
     </Shell>
   )
