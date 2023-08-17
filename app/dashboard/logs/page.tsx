98185d3eb8980ecb111ca711ad3ffe4b33fa0f8f
redpangilinan <redpangilinan715@gmail.com>
2023-08-18 01:19:58 +0800

    feat: create activity logs temporary table and data fetch

--- app/dashboard/logs/page.tsx
+++ app/dashboard/logs/page.tsx
@@ -1,8 +1,10 @@
 import { Metadata } from "next"
 import { redirect } from "next/navigation"
 
+import { db } from "@/lib/db"
 import { authOptions } from "@/lib/auth"
 import { getCurrentUser } from "@/lib/session"
+
 import { DashboardHeader } from "@/components/pages/dashboard/dashboard-header"
 import { Shell } from "@/components/layout/shell"
 
@@ -18,10 +20,54 @@ export default async function SettingsPage() {
     redirect(authOptions?.pages?.signIn || "/signin")
   }
 
+  const currentDate = new Date()
+  const sevenDaysAgo = new Date(currentDate)
+  sevenDaysAgo.setDate(currentDate.getDate() - 7)
+
+  const logs = await db.activityLog.findMany({
+    select: {
+      id: true,
+      date: true,
+      count: true,
+      activity: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    where: {
+      date: {
+        gte: sevenDaysAgo.toISOString(),
+        lte: currentDate.toISOString(),
+      },
+    },
+    orderBy: {
+      date: "desc",
+    },
+  })
+
   return (
     <Shell>
       <DashboardHeader heading="Logs" text="View and modify activity logs." />
-      <div className="grid grid-cols-1 gap-10">Work in progress</div>
+      <div className="grid grid-cols-1 gap-10">
+        <div className="overflow-x-auto">
+          <table className="min-w-full divide-y">
+            <tbody className="divide-y">
+              {logs.map((log) => (
+                <tr key={log.id}>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {new Date(log.date).toLocaleDateString()}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {log.activity.name}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">{log.count}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </Shell>
   )
 }
