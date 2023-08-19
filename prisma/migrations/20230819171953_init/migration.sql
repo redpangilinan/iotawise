b1c8199f595464553d1ff2cfa88675c553570b40
redpangilinan <redpangilinan715@gmail.com>
2023-08-20 02:50:06 +0800

    chore: migrations

--- prisma/migrations/20230819171953_init/migration.sql
+++ prisma/migrations/20230819171953_init/migration.sql
@@ -71,8 +71,8 @@ CREATE TABLE `activities` (
 CREATE TABLE `activity_log` (
     `id` VARCHAR(191) NOT NULL,
     `activity_id` VARCHAR(191) NOT NULL,
-    `date` DATETIME(3) NOT NULL,
-    `count` INTEGER NOT NULL,
+    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `count` INTEGER NOT NULL DEFAULT 1,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
