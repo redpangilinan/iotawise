060155f500f4dcd8c8b69a09cdc95f272c862818
redpangilinan <redpangilinan715@gmail.com>
2023-08-20 00:12:11 +0800

    feat: add empty placeholder for logs and fix formatting

--- app/dashboard/logs/page.tsx
+++ app/dashboard/logs/page.tsx
@@ -1,13 +1,18 @@
 import { Metadata } from "next"
 import { redirect } from "next/navigation"
+import Link from "next/link"
 
 import { db } from "@/lib/db"
 import { authOptions } from "@/lib/auth"
 import { getCurrentUser } from "@/lib/session"
 import { formatDate } from "@/lib/utils"
 
+import { EmptyPlaceholder } from "@/components/empty-placeholder"
 import { DashboardHeader } from "@/components/pages/dashboard/dashboard-header"
 import { Shell } from "@/components/layout/shell"
+import { Icon } from "@/components/icons"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
 
 export const metadata: Metadata = {
   title: "Logs",
@@ -50,23 +55,42 @@ export default async function SettingsPage() {
   return (
     <Shell>
       <DashboardHeader heading="Logs" text="View activity logs. (WIP)" />
-      <div className="overflow-x-auto">
-        <table className="min-w-full divide-y">
-          <tbody className="divide-y">
-            {logs.map((log) => (
-              <tr key={log.id}>
-                <td className="whitespace-nowrap px-6 py-4">
-                  {formatDate(log.date.toDateString())}
-                </td>
-                <td className="whitespace-nowrap px-6 py-4">
-                  {log.activity.name}
-                </td>
-                <td className="whitespace-nowrap px-6 py-4">{log.count}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {logs?.length ? (
+        <div className="overflow-x-auto">
+          <table className="min-w-full divide-y">
+            <tbody className="divide-y">
+              {logs.map((log) => (
+                <tr key={log.id}>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {formatDate(log.date.toDateString())}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {log.activity.name}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">{log.count}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <EmptyPlaceholder>
+          <div className="flex h-20 w-20 items-center justify-center rounded-full bg-muted">
+            <Icon name="history" className="h-10 w-10" />
+          </div>
+          <EmptyPlaceholder.Title>No logs yet</EmptyPlaceholder.Title>
+          <EmptyPlaceholder.Description>
+            You can add logs in the activities page.
+          </EmptyPlaceholder.Description>
+          <Link
+            href="/dashboard/activities"
+            className={`${cn(buttonVariants({ variant: "outline" }))}`}
+          >
+            <Icon name="activity" className="mr-2 h-4 w-4" />
+            Activities
+          </Link>
+        </EmptyPlaceholder>
+      )}
     </Shell>
   )
 }
