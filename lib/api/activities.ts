8e3301398b955576bc4e8e8c6edf7634e468b6fe
redpangilinan <redpangilinan715@gmail.com>
2023-09-03 22:45:16 +0800

    fix: fetch user activities promise typing

--- lib/api/activities.ts
+++ lib/api/activities.ts
@@ -3,6 +3,10 @@ import { authOptions } from "@/lib/auth"
 import { Activity, User } from "@prisma/client"
 import { db } from "@/lib/db"
 
+type UserActivities = Activity & {
+  total_count: number
+}
+
 // Fetch user's activity
 export async function getUserActivity(
   activityId: Activity["id"],
@@ -17,25 +21,32 @@ export async function getUserActivity(
 }
 
 // Fetch all of the activities for the selected user
-export async function getUserActivities(userId: string): Promise<Activity[]> {
-  return await db.$queryRaw`
-  SELECT
-    A.id,
-    A.name,
-    A.description,
-    A.color_code AS 'colorCode',
-    A.created_at AS 'createdAt',
-    SUM(AL.count) AS total_count
-  FROM
-    activities A
-  LEFT JOIN
-    activity_log AL ON A.id = AL.activity_id
-  WHERE
-    A.user_id = ${userId}
-  GROUP BY
-    A.id, A.name, A.description, A.color_code
-  ORDER BY
-    total_count DESC;`
+export async function getUserActivities(
+  userId: string
+): Promise<UserActivities[]> {
+  const results: UserActivities[] = await db.$queryRaw`
+    SELECT
+      A.id,
+      A.name,
+      A.description,
+      A.color_code AS 'colorCode',
+      A.created_at AS 'createdAt',
+      SUM(AL.count) AS total_count
+    FROM
+      activities A
+    LEFT JOIN
+      activity_log AL ON A.id = AL.activity_id
+    WHERE
+      A.user_id = ${userId}
+    GROUP BY
+      A.id, A.name, A.description, A.color_code
+    ORDER BY
+      total_count DESC;`
+
+  return results.map((result) => ({
+    ...result,
+    total_count: Number(result.total_count),
+  }))
 }
 
 // Verify if the user has access to the activity
