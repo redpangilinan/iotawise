c911acf1b19c235c5ff74aa795c46398da3fd58e
redpangilinan <redpangilinan715@gmail.com>
2023-09-29 19:08:44 +0800

    refactor: router -> redirect

--- components/pwa-wrapper.tsx
+++ components/pwa-wrapper.tsx
@@ -1,22 +1,19 @@
 "use client"
 
 import * as React from "react"
-import { useRouter } from "next/navigation"
+import { redirect } from "next/navigation"
 
 interface PWAWrapperProps {
   children: React.ReactNode
 }
 
 export function PWAWrapper({ children }: PWAWrapperProps) {
-  const router = useRouter()
-
   // Redirect to sign in if PWA
   React.useEffect(() => {
     if (window.matchMedia("(display-mode: standalone)").matches) {
-      router.push("/signin")
-      router.refresh()
+      redirect("/signin")
     }
-  }, [router])
+  }, [])
 
   return <main>{children}</main>
 }
