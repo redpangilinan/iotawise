1a4fd96e9ec2efeca1d2c7450f67a0864b0ff036
redpangilinan <redpangilinan715@gmail.com>
2023-10-07 17:59:56 +0800

    fix: og image metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,12 +33,20 @@ export const metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [`${siteConfig.url.base}/og.jpg`],
+    images: [siteConfig.ogImage],
     creator: "@_rdev7",
   },
   icons: {
