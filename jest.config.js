d13c096d472c9ad723f3ebd06035fbe549c35db0
redpangilinan <redpangilinan715@gmail.com>
2023-11-23 22:19:37 +0800

    chore: setup jest tests

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require("next/jest")
+
+const createJestConfig = nextJest({
+  dir: "./",
+})
+
+const customJestConfig = {
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+}
+
+module.exports = createJestConfig(customJestConfig)
