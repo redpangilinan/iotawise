344b4a04e09946b57c9fe2d4e817413df2c5837b
redpangilinan <redpangilinan715@gmail.com>
2024-01-02 00:05:01 +0800

    feat: add github star count to opensource section

--- components/pages/opensource.tsx
+++ components/pages/opensource.tsx
@@ -1,15 +1,24 @@
 import Link from "next/link"
-import { FaGithub } from "react-icons/fa"
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import HeadingText from "@/components/heading-text"
+import { Icons } from "@/components/icons"
 
-export default function OpenSource() {
+export default async function OpenSource() {
+  const { stargazers_count: stars } = await fetch(
+    "https://api.github.com/repos/redpangilinan/iotawise",
+    {
+      next: { revalidate: 60 },
+    }
+  )
+    .then((res) => res.json())
+    .catch((e) => console.error(e))
+
   return (
     <section className="container py-12 lg:py-20">
-      <div className="flex flex-col items-center gap-8">
+      <div className="flex flex-col items-center gap-4">
         <HeadingText
           subtext="Feel free to view the codebase or contribute!"
           className="text-center"
@@ -19,12 +28,10 @@ export default function OpenSource() {
         <Link
           href={siteConfig.links.github}
           target="_blank"
-          className={`w-[10rem] gap-2 ${cn(
-            buttonVariants({ variant: "outline", size: "sm" })
-          )}`}
+          className={cn(buttonVariants({ variant: "outline" }))}
         >
-          <FaGithub />
-          Github
+          <Icons.star className="mr-2 h-4 w-4" />
+          <span>{stars} on Github</span>
         </Link>
       </div>
     </section>
